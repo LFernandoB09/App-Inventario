755cb51b6f2e96df838a8c0ee7cfb374c7235ecb
LFernandoB09 <bautistaalcozer.luisfernando36@gmail.com>
2024-04-28 12:02:16 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace App_Inventario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContr = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(187, 161);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // txtContr
+            // 
+            this.txtContr.Location = new System.Drawing.Point(187, 208);
+            this.txtContr.Name = "txtContr";
+            this.txtContr.Size = new System.Drawing.Size(100, 20);
+            this.txtContr.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 390);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtContr);
+            this.Controls.Add(this.txtUsuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContr;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ funciones.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace App_Inventario
+{
+    internal class funciones
+    {
+        //static string cadena_conexion = "Server = localhost; User = root; Password = ; Database = cooler";
+        //MySqlConnection conexion = new MySqlConnection(cadena_conexion);
+
+        public static void login(string usuario, string contr)
+        {
+            string cadena_conexion = "Server = localhost; User = root; Password = ; Database = cooler";
+            MySqlConnection conexion = new MySqlConnection(cadena_conexion);
+            try
+            {
+                conexion.Open();
+                string consulta = "SELECT Usuario, Password FROM usuarios WHERE Usuario = @usuario AND Password = @contr";
+                MySqlCommand cmd = new MySqlCommand(consulta, conexion);
+                cmd.Parameters.AddWithValue("@usuario", usuario);
+                cmd.Parameters.AddWithValue("@contr", contr);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if(reader.Read())
+                {
+                    string username = reader["Usuario"].ToString();
+                    MessageBox.Show("Bienvenido " + username);
+                    Form2 form2 = new Form2();
+                    form2.Show();
+                }
+                else
+                {
+                    MessageBox.Show("El usuario no existe en la base de datos");
+                }
+                reader.Close();
+                conexion.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+            finally
+            {
+                if (conexion.State == ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+        }
+    }
+}
